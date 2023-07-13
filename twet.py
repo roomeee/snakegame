0175a2412c1d4df8d9e1d406f84eda88de57113a
Rooma <110466640+roomeee@users.noreply.github.com>
2023-07-12 21:27:10 -1100

    Update twet.py

--- twet.py
+++ twet.py
@@ -1,19 +1,16 @@
 import turtle
 
-# Set up the screen
 wn = turtle.Screen()
 wn.bgcolor("black")
 
-# Create the turtle
 spiral_turtle = turtle.Turtle()
 spiral_turtle.speed(0)  # Fastest speed
 spiral_turtle.color("white")
 spiral_turtle.width(2)
 
-# Define the parameters for the spiral
 start_radius = 1  # Starting radius
 angle = 91  # Angle between each line segment
-multiplier = 1.02  # Increase in radius with each line segment
+multiplier = 1.02  
 
 # Draw the spiral
 for i in range(400):
@@ -21,7 +18,6 @@ for i in range(400):
     spiral_turtle.left(angle)
     start_radius *= multiplier
 
-# Hide the turtle
 spiral_turtle.hideturtle()
 
 # Exit on click
